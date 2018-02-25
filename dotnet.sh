75371d985613b8e68fffe87ed6b35d4ac6331e85
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-02-25 13:58:39 -0500

    dotnet core script for ubuntu 16.04

---
+++ dotnet.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Script for installing dotnet core 2.X on Ubuntu 16.04
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.1.4
+dotnet --version
