fcdcaf9e304ac6a07344fe5cae306d3a3e5534c8
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-03-11 14:13:46 -0400

    utils script for grabbing enode id, node port (rpc) and public ip address

---
+++ masternode/utils.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Logic to handle arguments for optionally creating systemd service
+for i in "$@"
+do
+case $i in
+    -e|--enodeId)
+    echo 'Enode Id:' $(journalctl -u masternode.service | grep 'UDP listener up' | awk '{print $11}' | grep -o -P '(?<=node://).*(?=@)')
+    shift
+    ;;
+    -p|--nodePort)
+    echo 'Node Port:' $(journalctl -u masternode.service | grep 'HTTP endpoint opened' | awk '{print $11}' | awk '{print $1}' | grep -o -P '(?<=http://0.0.0.0:).*')
+    shift
+    ;;
+    -i|--nodeIp)
+    echo 'Node IP:' $(curl --silent icanhazip.com)
+    shift
+    ;;
+    -h|--help)
+    echo '-e or --enodeId prints out EnodeID tied to geth instance'
+    echo '-p or --nodePort prints out the rpc port tied to geth instance'
+    echo '-i or --nodeIp prints out the public facing IP address for this server'
+    shift
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
